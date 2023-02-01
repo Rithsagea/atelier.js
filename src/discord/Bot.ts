f3f38c30049931f448ea5caa4384c2d083346712
Rithsagea <rithsagea@gmail.com>
2023-01-31 21:01:27 -0500

    .

--- src/discord/Bot.ts
+++ src/discord/Bot.ts
@@ -1,26 +1,14 @@
-import { Client } from "discord.js";
+import { Client, Interaction } from "discord.js";
 
-export class Bot {
-  readonly client = new Client({
-    intents: [],
-  });
+const client = new Client({
+  intents: [],
+});
 
-  constructor() {
-    let client = this.client;
-  }
+client.on("ready", async () => {
+  if (!client.user || !client.application) return;
+  console.log(`${client.user.username} is online!`);
+});
 
-  ready() {
-    console.log("Bot is starting...");
-
-    let client = this.client;
-
-    client.on("ready", async () => {
-      if (!client.user || !client.application) return;
-      console.log(`${client.user.username} is online!`);
-    });
-  }
-
-  login() {
-    this.client.login(process.env.BOT_TOKEN);
-  }
+export function login() {
+  client.login(process.env.BOT_TOKEN);
 }
